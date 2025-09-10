f774852ed8ba3b8d2a65a601923c8585b4ed6a28
erikwilbert <erik.wilbert@ui.ac.id>
2025-09-10 14:01:22 +0700

    tutorial 2 selesai

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "erik-wilbert-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://pbp.cs.ui.ac.id"
+    "https://erik-wilbert-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
